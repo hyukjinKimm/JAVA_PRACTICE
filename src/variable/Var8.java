2b0a8ac760256b47c3c2a74609d1fb03e12a6e96
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2024-01-28 18:34:13 +0900

    변수타입

---
+++ src/variable/Var8.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+        byte b = 127; // -128 ~ +127
+        short s = 23767; // -32768 ~ +32787
+        int i = 14;
+        long l = 123L;
+
+        //실수
+        float f = 10.0f;
+        double d = 10.0;
+
+
+    }
+}
