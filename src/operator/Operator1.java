c4af7c7c5d232f6515cd715547ec4ae3fb51f458
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2024-01-29 16:20:21 +0900

    산술 연산자

---
+++ src/operator/Operator1.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class Operator1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        System.out.println(b-a);
+        
+        int div = a / b ;
+        System.out.println("div = " + div);
+        int mod = a % b;
+        System.out.println("mod = " + mod);
+        int c = 5 / 0;
+    }
+}
