d0e85a713db61af246de29941edbbf6937220117
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2024-01-29 17:14:02 +0900

    증감 연산자

---
+++ src/operator/OperatorAdd1.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorAdd1 {
+    public static void main(String[] args) {
+        int a = 0;
+
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a);
+
+        System.out.println(--a);
+    }
+}
