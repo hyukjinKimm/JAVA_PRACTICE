40d6e64b61905ba2a47c2360710968dfce2912b4
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2024-01-30 16:41:23 +0900

    논리 연산자

---
+++ src/operator/Logical1.java
@@ -0,0 +1,19 @@
+package operator;
+
+public class Logical1 {
+    public static void main(String[] args) {
+        System.out.println("&& AND 연산");
+        System.out.println(true && true);
+        System.out.println(false && true);
+
+
+        System.out.println("||: OR 연산");
+        System.out.println(false || true);
+        System.out.println(false || true);
+
+        System.out.println("! 연산");
+        System.out.println(!true);
+        System.out.println(!false);
+
+    }
+}
