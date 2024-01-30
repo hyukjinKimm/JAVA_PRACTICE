57d4024e976d1d4318b2a614ec96909fb0f31a32
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2024-01-30 21:31:05 +0900

    삼항 연산자

---
+++ src/cond/CondOp2.java
@@ -0,0 +1,10 @@
+package cond;
+
+public class CondOp2 {
+    public static void main(String[] args) {
+
+        int age = 18;
+        String result = (age >= 20) ? "맞다" : "아니다";
+        System.out.println(result);
+    }
+}
