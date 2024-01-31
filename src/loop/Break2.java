0afcf001c061fbcc154a239a6bf297139e54c451
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2024-01-31 16:16:58 +0900

    for문2

---
+++ src/loop/Break2.java
@@ -0,0 +1,14 @@
+package loop;
+
+public class Break2 {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int i = 1;
+        for (;;) {
+            sum += i;
+            if(sum>10)break;
+        }
+        System.out.println(sum);
+    }
+}
