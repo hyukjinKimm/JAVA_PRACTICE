81103e20a44e4ad7a2e64f1332eb00c92d72b12f
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2024-02-06 22:42:23 +0900

    메서드 정의

---
+++ src/method/Method2.java
@@ -0,0 +1,11 @@
+package method;
+
+public class Method2 {
+    public static void main(String[] args) {
+        printHello();
+    }
+    public static void printHello(){
+        System.out.println("프로그램 시작");
+
+    }
+}
